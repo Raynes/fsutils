c489fc85cc59e649d74ed0e3839a5ebd36e82468
Anthony Grimes <i@raynes.me>
2012-11-04 03:37:34 -0600

    Add walk.

--- src/System/Path.hs
+++ src/System/Path.hs
@@ -1,18 +1,15 @@
-module System.Path (mtreeList,
-                    fileList)
+module System.Path ( mtreeList
+                   , fileList )
 where
                      
-import Control.Monad (liftM)
-import System.Directory (getDirectoryContents, doesDirectoryExist)
+import Control.Monad (liftM, filterM)
+import System.Directory (getDirectoryContents, doesDirectoryExist, doesFileExist)
 import System.FilePath ((</>))
-
--- | Checks if a path is '.' or '..'.
-uselessPath :: FilePath -> Bool
-uselessPath path = path == "." || path == ".."
+import Data.List ((\\))
 
 -- | Remove useless paths from a list of paths.
 filterUseless :: [FilePath] -> [FilePath]
-filterUseless = filter $ not . uselessPath
+filterUseless = (\\ [".", ".."])
 
 -- | Returns a list of nodes in a tree via a depth-first walk.
 mtreeList :: Monad m => (a -> m [a]) -> a -> m [a]
@@ -22,11 +19,41 @@ mtreeList children root = do
   joined <- sequence subChildren
   return $ root : concat joined
 
+topFileList :: FilePath -> IO [FilePath]
+topFileList path = liftM filterUseless (getDirectoryContents path) >>= return . map (path </>)
+
 -- | Recursively list the contents of a directory. Depth-first.
 fileList :: FilePath -> IO [FilePath]
 fileList root = mtreeList children root
   where children path = do
           directory <- doesDirectoryExist path
           if directory
-            then liftM filterUseless (getDirectoryContents path) >>= return . map (path </>)
+            then topFileList path
             else return []
+
+-- | We can use this data type to represent the pieces of a directory.
+data Directory = Directory
+                 { -- | The path of the directory itself.
+                   dirPath :: FilePath
+                   -- | All subdirectories of this directory.
+                 , subDirs :: [FilePath]
+                   -- | All files contained in this directory.
+                 , files   :: [FilePath]
+                 }
+               deriving (Show)
+
+-- | Creates a Directory instance from a FilePath.
+createDir :: FilePath -> IO Directory
+createDir path = do
+  contents <- topFileList path
+  subdirs  <- filterM doesDirectoryExist contents
+  files    <- filterM doesFileExist contents
+  return (Directory path subdirs files)
+  
+-- | Walk a directory depth-first. Similar to Python's os.walk and fs.core/walk
+-- from the fs Clojure library.
+walkDir :: FilePath -> IO [Directory]
+walkDir root = createDir root >>= mtreeList children
+  where children path = do
+          let dirs = subDirs path
+          mapM createDir dirs
