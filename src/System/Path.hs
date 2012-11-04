a16bd0ed5400469dd4df569a88635c2f0c5c0544
Anthony Grimes <i@raynes.me>
2012-11-03 23:05:27 -0500

    Added a modified version of amalloy's mtreeSeq (now mtreeList) and fileSeq (fileList).

--- src/System/Path.hs
+++ src/System/Path.hs
@@ -1,17 +1,33 @@
-import Control.Monad
-import System.Directory
+module System.Path (mtreeList,
+                    fileList)
+where
+                     
+import Control.Monad (liftM)
+import System.Directory (getDirectoryContents, doesDirectoryExist)
 import System.FilePath ((</>))
+import Data.List ((\\))
 
+-- | Checks if a path is '.' or '..'.
 uselessPath :: FilePath -> Bool
 uselessPath path = path == "." || path == ".."
 
-recurseDir :: FilePath -> IO [FilePath]
-recurseDir path = do
-  names <- liftM (filter $ not . uselessPath) $ getDirectoryContents path
-  paths <- forM names $ \name -> do
-    let dir = path </> name
-    isDir <- doesDirectoryExist dir
-    if isDir
-      then recurseDir dir >>= return . ((:) dir)
-      else return [dir]
-  return (concat paths)
+-- | Remove useless paths from a list of paths.
+filterUseless :: [FilePath] -> [FilePath]
+filterUseless = filter $ not . uselessPath
+
+-- | Returns a list of nodes in a tree via a depth-first walk.
+mtreeList :: Monad m => (a -> m [a]) -> a -> m [a]
+mtreeList children root = do
+  xs <- children root
+  let subChildren = map (mtreeList children) xs
+  joined <- sequence subChildren
+  return $ root : concat joined
+
+-- | Recursively list the contents of a directory. Depth-first.
+fileList :: FilePath -> IO [FilePath]
+fileList root = mtreeList children root
+  where children path = do
+          directory <- doesDirectoryExist path
+          if directory
+            then liftM filterUseless (getDirectoryContents path) >>= return . map (path </>)
+            else return []
